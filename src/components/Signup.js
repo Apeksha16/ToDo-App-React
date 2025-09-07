2dca6ff75b8bccbd49f6f0795ead585fc82d8090
Apeksha Verma <apekshaverma@Apekshas-MacBook-Air.local>
2025-09-07 22:25:43 +0530

    validations set - user creation

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,15 +1,26 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 const Signup = () => {
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  
+  const navigate = useNavigate();
 
-  const isNameValid = fullName.trim().length > 4;
-  const isEmailValid = email.endsWith("@gmail.com");
-  const isPasswordValid = password.length >= 6;
-  const isFormValid = isEmailValid && isNameValid && isPasswordValid;
+
+  const checkValidations = () => {
+    if (fullName.length < 4) {
+      return false;
+    }
+    if (!email.endsWith("@gmail.com")) {
+      return false;
+    }
+    if (password.length < 4) {
+      return false;
+    }
+    return true;
+  };
 
   return (
     <div className="bg-amber-50 flex flex-col gap-8 rounded-xl p-14 items-center">
@@ -18,27 +29,43 @@ const Signup = () => {
       <input
         className="w-xs h-14 p-4  border-2 border-solid rounded-md focus:border-orange-400"
         type="text"
-        placeholder="Full Name" value={fullName} onChange={(e)=> setFullName(e.target.value)}
+        placeholder="Full Name"
+        value={fullName}
+        onChange={(e) => {
+          setFullName(e.target.value);
+        }}
       />
       <input
         className="w-xs h-14 p-4  border-2 border-solid rounded-md focus:border-orange-400"
         type="text"
-        placeholder="Email Address" value={email} onChange={(e)=> setEmail(e.target.value)}
+        placeholder="Email Address"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
       />
       <input
         className="w-xs h-14 p-4  border-2 border-solid rounded-md focus:border-orange-400"
         type="password"
-        placeholder="Create Password" value={password} onChange={(e)=> setPassword(e.target.value)}
+        placeholder="Create Password"
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
       />
 
-      <button disabled={!isFormValid} className={`w-xs h-14 rounded-3xl text-white ${
-          isFormValid
+   <button disabled={!checkValidations()} className={`w-xs h-14 rounded-3xl text-white ${
+          checkValidations()
             ? "bg-orange-950 hover:bg-orange-800 cursor-pointer"
             : "bg-gray-400 cursor-not-allowed"
         }`}
+        onClick={()=>{
+            navigate("/");
+        }}
     >
         Get Started
-      </button>
+     </button>
+     
       <p className="text-orange-950">
         <Link to="/"> Already have an account?</Link>
       </p>
