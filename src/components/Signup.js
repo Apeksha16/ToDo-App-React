c5eb6f7a3ac6fadd54d0587b8c6e0a1a1e2bd98d
Apeksha Verma <apekshaverma@Apekshas-MacBook-Air.local>
2025-09-10 17:53:03 +0530

    register API set

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,28 +1,48 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { callApi } from "../utils/api";
 
 const Signup = () => {
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-const [error, setError]=useState("");
-  
-  const navigate = useNavigate();
+  const [error, setError] = useState("");
 
+  const navigate = useNavigate();
 
   const checkValidations = () => {
     if (fullName.length < 4) {
- setError("Full Name should contain atleast 4 characters.");
-    }
-    else if (!email.endsWith("@gmail.com")) {
- setError("Enter a valid Gmail address.");
-    }
-    else if (password.length < 4) {
- setError("Password must be at least 4 characters.");
-    }else {
+      setError("Full Name should contain atleast 4 characters.");
+    } else if (!email.endsWith("@email.com")) {
+      setError("Enter a valid Gmail address.");
+    } else if (password.length < 4) {
+      setError("Password must be at least 4 characters.");
+    } else {
       setError("");
-      navigate("/");
+      // navigate("/");
+      handleSignup();
     }
+  };
+
+const handleSignup =async()=>{
+  let req = {fullName, email, password};
+  try{
+  const getResponseFromApi = await callApi(
+  "https://todoapp-f6d7.onrender.com/api/auth/register", 
+  { name: fullName, email, password }
+);
+
+    console.log(getResponseFromApi);
+if(getResponseFromApi.status ===true && getResponseFromApi.data.status === true){
+  navigate("/");
+} else if(getResponseFromApi.status===false){
+  setError("Something went wrong, Please try again!");
+} else {
+  setError(getResponseFromApi.data.message);
+}
+  } catch(err){
+    setError("Error connecting to server.");
+  }
 };
 
   return (
@@ -56,14 +76,15 @@ const [error, setError]=useState("");
           setPassword(e.target.value);
         }}
       />
-            {error && <p className="text-red-500">{error}</p>}
+      {error && <p className="text-red-500">{error}</p>}
 
-    <button className="w-xs h-14 rounded-3xl text-white bg-orange-950 hover:bg-orange-800 cursor-pointer"
-      onClick={checkValidations}
+      <button
+        className="w-xs h-14 rounded-3xl text-white bg-orange-950 hover:bg-orange-800 cursor-pointer"
+        onClick={checkValidations}
       >
         Get Started
-     </button>
-     
+      </button>
+
       <p className="text-orange-950">
         <Link to="/"> Already have an account?</Link>
       </p>
